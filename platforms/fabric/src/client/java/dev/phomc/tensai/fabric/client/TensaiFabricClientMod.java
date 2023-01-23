9eeb2aee48ef36de673fb7be008eb0298ac80589
Just Mango <38831897+JustMangoT@users.noreply.github.com>
2023-01-23 10:23:43 +0700

    Add client

---
+++ platforms/fabric/src/client/java/dev/phomc/tensai/fabric/client/TensaiFabricClientMod.java
@@ -0,0 +1,9 @@
+package dev.phomc.tensai.fabric.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TensaiFabricClientMod implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
