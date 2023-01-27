3e1accf18573b1171ca0a80be4a5bd1cf29d691d
anhcraft <huynhduyanh123123@gmail.com>
2023-01-27 20:10:45 +0700

    Fix build configuration

--- platforms/bukkit/build.gradle.kts
+++ platforms/bukkit/build.gradle.kts
@@ -1,5 +1,18 @@
 dependencies {
     implementation(project(":tensai-common"))
-
+    implementation("org.slf4j:slf4j-api:1.7.25")
     compileOnly("org.spigotmc:spigot-api:${property("minecraftVersion")}-R0.1-SNAPSHOT")
+}
+
+tasks {
+    register("bukkitFatJar", Jar::class.java) {
+        mustRunAfter(build)
+        dependsOn(jar)
+        archiveClassifier.set("fat")
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        from(configurations.compileClasspath.get()
+            .filter {it.name.equals("main") }
+            .map { if (it.isDirectory) it else zipTree(it) })
+        from(sourceSets.main.get().output)
+    }
 }
