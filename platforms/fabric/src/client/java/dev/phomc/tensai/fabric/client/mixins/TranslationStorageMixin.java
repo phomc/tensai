e62269e6e6c45abf480ef1cc9bedf6245083cd37
anhcraft <huynhduyanh123123@gmail.com>
2023-01-28 08:56:07 +0700

    Add custom translation utility

---
+++ platforms/fabric/src/client/java/dev/phomc/tensai/fabric/client/mixins/TranslationStorageMixin.java
@@ -0,0 +1,55 @@
+/*
+ * This file is part of tensai, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2023 PhoMC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package dev.phomc.tensai.fabric.client.mixins;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.client.resource.language.TranslationStorage;
+
+import dev.phomc.tensai.fabric.client.i18n.CustomTranslationStorage;
+
+@Mixin(TranslationStorage.class)
+public abstract class TranslationStorageMixin {
+	@Inject(method = "get(Ljava/lang/String;)Ljava/lang/String;", at = @At("RETURN"), cancellable = true)
+	private void getCustomTranslation(String key, CallbackInfoReturnable<String> cir) {
+		String s = CustomTranslationStorage.getInstance().get(key);
+
+		if (s != null) {
+			cir.setReturnValue(s);
+		}
+	}
+
+	@Inject(method = "hasTranslation(Ljava/lang/String;)Z", at = @At("RETURN"), cancellable = true)
+	private void hasCustomTranslation(String key, CallbackInfoReturnable<Boolean> cir) {
+		String s = CustomTranslationStorage.getInstance().get(key);
+
+		if (s != null) {
+			cir.setReturnValue(true);
+		}
+	}
+}
